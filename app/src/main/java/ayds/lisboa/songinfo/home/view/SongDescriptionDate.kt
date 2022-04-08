9a8b531515665f15ad78b59bdd1a7ebc22b18ce4
agusL1660 <lelliag@hotmail.com>
2022-04-08 16:53:54 -0300

    Created SongDescriptionDate and update SongDescriptionHelperImpl

---
+++ app/src/main/java/ayds/lisboa/songinfo/home/view/SongDescriptionDate.kt
@@ -0,0 +1,54 @@
+package ayds.lisboa.songinfo.home.view
+
+import ayds.lisboa.songinfo.home.model.entities.EmptySong
+import ayds.lisboa.songinfo.home.model.entities.Song
+
+interface SongDescriptionDate {
+    fun getReleaseDataByPrecision(song: Song = EmptySong): String
+}
+
+internal class SongDescriptionDateImpl : SongDescriptionDate {
+    override fun getReleaseDataByPrecision(song: Song): String {
+        return when(song.releaseDatePrecision){
+             "day" -> {
+                 getDateWithDayPrecision(song.releaseDate)
+             }
+             "month" -> {
+                 getDateWithMonthPrecision(song.releaseDate)
+             }
+             "year" -> {
+                 if(isLeapYear(song.releaseDate.toInt())){
+                     getDateWithYearPrecision(song.releaseDate) + " (a leap year)"
+                 } else {getDateWithYearPrecision(song.releaseDate) + " (not a leap year)"}
+             }
+
+            else -> {
+                "Date not found"
+            }
+        }
+    }
+
+    private fun getDateWithDayPrecision(date: String): String {
+        val arrayDate = date.split("-")
+        return arrayDate[2] +"/"+ arrayDate[1]+"/" +arrayDate[0]
+    }
+
+    private fun getDateWithMonthPrecision(date: String): String {
+        val arrayDate = date.split("-")
+        val months = arrayOf("January","February","March","April","May","June","July","August","September","October","November","December")
+
+        return months[arrayDate[1].toInt()-1]+", " +arrayDate[0]
+    }
+
+    private fun isLeapYear(n: Int) = (n % 4 == 0) && (n % 100 != 0 || n % 400 == 0)
+
+    private fun getDateWithYearPrecision(date: String): String {
+        return date.split("-").first()
+    }
+}
+
+
+
+
+
+
--- app/src/main/java/ayds/lisboa/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/lisboa/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -10,6 +10,7 @@ interface SongDescriptionHelper {
 
 internal class SongDescriptionHelperImpl : SongDescriptionHelper {
     override fun getSongDescriptionText(song: Song): String {
+        val songDescription: SongDescriptionDate=SongDescriptionDateImpl()
         return when (song) {
             is SpotifySong ->
                 "${
@@ -18,50 +19,9 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 }\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                            if(isPresicionDay(song.releaseDate))
-                                    "Release date: ${paraDia(song.releaseDate)}"
-                            else
-                               if(isPresicionMonth(song.releaseDate))
-                                    "Release date: ${paraMes(song.releaseDate)}"
-                               else
-                                    if(esBisiesto(song.releaseDate.toInt()))
-                                        "Release date: ${paraAnio(song.releaseDate)}" + "(a leap year)"
-                                    else
-                                        "Release date: ${paraAnio(song.releaseDate)}" + "(not a leap year)"
+                        "Release date: ${songDescription.getReleaseDataByPrecision(song)}"
 
-                       // "Year: ${song.year}"
             else -> "Song not found"
         }
     }
-    private fun isPresicionDay(date: String): Boolean {
-        return date=="day"
-    }
-
-    private fun isPresicionMonth(date: String): Boolean {
-        return date=="month"
-    }
-    private fun paraDia(date: String): String {
-        var arregloFecha = date.split("-")
-        return arregloFecha[2] +"/"+ arregloFecha[1]+"/" +arregloFecha[0]
-    }
-
-    private fun paraMes(date: String): String {
-        var arregloFecha = date.split("-")
-        var Meses = arrayOf("January","February","March","April","May","June","July","August","September","October","November","December")
-        var resultado: String= Meses[arregloFecha[1].toInt()]+", " +arregloFecha[0]
-
-        return resultado
-    }
-
-    private fun esBisiesto(n: Int) = (n % 4 == 0) && (n % 100 != 0 || n % 400 == 0)
-
-    private fun paraAnio(date: String): String {
-        return date.split("-").first()
-    }
-
-
-
-
-
-
 }
