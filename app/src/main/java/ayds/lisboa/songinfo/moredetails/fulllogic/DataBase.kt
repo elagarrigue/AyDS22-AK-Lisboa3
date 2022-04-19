49054ce8b5e34e1d79c65259f38faffb009ee49b
Ignacio Dotta <ij.dotta@gmail.com>
2022-04-19 12:03:19 -0300

    Refactor saveArtist function

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/fulllogic/DataBase.kt
@@ -75,30 +75,28 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         }
 
         fun saveArtist(dbHelper: DataBase, artist: String?, info: String?) {
-            // Gets the data repository in write mode
             val db = dbHelper.writableDatabase
 
-// Create a new map of values, where column names are the keys
-            val values = ContentValues()
-            values.put("artist", artist)
-            values.put("info", info)
-            values.put("source", 1)
+            val values = ContentValues().apply {
+                put(ARTIST_COLUMN, artist)
+                put(INFO_COLUMN, info)
+                put(SOURCE_COLUMN, 1)
+            }
 
-// Insert the new row, returning the primary key value of the new row
-            val newRowId = db.insert("artists", null, values)
+            db.insert(ARTISTS_TABLE, null, values)
         }
 
         fun getInfo(dbHelper: DataBase, artist: String): String? {
             val db = dbHelper.readableDatabase
 
-// Define a projection that specifies which columns from the database
-// you will actually use after this query.
+            // Define a projection that specifies which columns from the database
+            // you will actually use after this query.
 
-// Filter results WHERE "title" = 'My Title'
+            // Filter results WHERE "title" = 'My Title'
             val selection = "artist  = ?"
             val selectionArgs = arrayOf(artist)
 
-// How you want the results sorted in the resulting Cursor
+        // How you want the results sorted in the resulting Cursor
             val sortOrder = "artist DESC"
             val cursor = db.query(
                 "artists",  // The table to query
