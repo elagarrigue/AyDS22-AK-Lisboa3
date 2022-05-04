7d05e1c946f97a3a8691f96c430fa4cb98983011
Ignacio Dotta <ijdotta@gmail.com>
2022-05-04 00:02:31 -0300

    LastFM external repository impl

---
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/lastfm/LastFMService.kt
@@ -0,0 +1,25 @@
+package ayds.lisboa.songinfo.moredetails.home.model.repository.external.lastfm
+
+import ayds.lisboa.songinfo.moredetails.home.model.entities.Artist
+import retrofit2.Response
+
+interface LastFMService {
+
+    fun getArtist(artistName: String): Artist?
+}
+
+class LastFMServiceImpl(
+    private val lastFMAPI: LastFMAPI,
+    private val lastFMToArtistResolver: LastFMToArtistResolver,
+): LastFMService {
+
+    override fun getArtist(artistName: String): Artist? {
+        val callResponse = getArtistFromService(artistName)
+        return lastFMToArtistResolver.getArtistFromExternalData(callResponse.body())
+    }
+
+    private fun getArtistFromService(artistName: String): Response<String> {
+        return lastFMAPI.getArtistInfo(artistName).execute()
+    }
+
+}
---
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/lastfm/LastFMToArtistResolver.kt
@@ -0,0 +1,50 @@
+package ayds.lisboa.songinfo.moredetails.home.model.repository.external.lastfm
+
+import ayds.lisboa.songinfo.moredetails.home.model.entities.LastFMArtist
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+
+interface LastFMToArtistResolver {
+    fun getArtistFromExternalData(serviceData: String?): LastFMArtist?
+}
+
+private const val ARTIST = "artist"
+private const val NAME = "name"
+private const val URL = "url"
+private const val BIO = "bio"
+private const val BIO_CONTENT = "content"
+
+internal class JsonToArtistResolver(
+    private val lastFMArtistBioParser: LastFMArtistBioParser
+): LastFMToArtistResolver {
+    override fun getArtistFromExternalData(serviceData: String?): LastFMArtist? =
+        try {
+            serviceData?.getArtistItem()?.let { item ->
+                LastFMArtist(
+                    item.getName(), item.getUrl(), item.getBioContent()
+                )
+            }
+        } catch (e: Exception) {
+            null
+        }
+
+    private fun String?.getArtistItem(): JsonObject {
+        val jobj = Gson().fromJson(this, JsonObject::class.java)
+        return jobj[ARTIST].asJsonObject
+    }
+
+    private fun JsonObject.getName(): String {
+        return this[NAME].asString
+    }
+
+    private fun JsonObject.getUrl(): String {
+        return this[URL].asString
+    }
+
+    private fun JsonObject.getBioContent(): String {
+        val bio = this[BIO].asJsonObject
+        val content = bio[BIO_CONTENT].asString
+        return lastFMArtistBioParser.parseArtistBioToDisplayableHtml(content, this.getName())
+    }
+
+}
