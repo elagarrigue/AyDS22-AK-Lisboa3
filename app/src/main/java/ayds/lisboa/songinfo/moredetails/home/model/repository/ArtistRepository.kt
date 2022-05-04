4bb94c4b772e13cf34cde369c7de6e83ea13debf
Thiago Zanconi <thiagzanc@gmail.com>
2022-05-04 12:22:03 -0300

    Implemented ArtistRepository (incomplete)

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/ArtistRepository.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/ArtistRepository.kt
@@ -1,2 +1,51 @@
 package ayds.lisboa.songinfo.moredetails.home.model.repository
 
+import ayds.lisboa.songinfo.home.model.entities.EmptySong
+import ayds.lisboa.songinfo.home.model.entities.SpotifySong
+import ayds.lisboa.songinfo.moredetails.home.model.entities.Artist
+import ayds.lisboa.songinfo.moredetails.home.model.entities.EmptyArtist
+import ayds.lisboa.songinfo.moredetails.home.model.entities.LastFMArtist
+import ayds.lisboa.songinfo.moredetails.home.model.repository.external.lastfm.LastFMService
+import ayds.lisboa.songinfo.moredetails.home.model.repository.local.lastfm.LastFMLocalStorage
+
+interface ArtistRepositoy {
+    fun getArtistByName(term: String): Artist
+}
+
+internal class ArtistRepositoryImpl(
+    private val lastFMLocalStorage: LastFMLocalStorage,
+    private val lastFMService: LastFMService
+) : ArtistRepositoy {
+
+    override fun getArtistByName(term: String): Artist {
+        var lastFMArtist = lastFMLocalStorage.getArtistByName(term)
+
+        when {
+            lastFMArtist != null -> markArtistAsLocal(lastFMArtist)
+            else -> {
+                try {
+                    lastFMArtist = lastFMService.getArtist(term)
+                    /*
+                    lastFMArtist?.let {
+                        when {
+                            it.isSavedArtist() -> lastFMLocalStorage.updateArtistTerm(term, it.id)
+                            else -> lastFMLocalStorage.insertArtist(term, it)
+                        }
+                    }
+                    */
+                } catch (e: Exception) {
+                    lastFMArtist = null
+                }
+            }
+        }
+
+        return lastFMArtist ?: EmptyArtist
+    }
+
+    //private fun LastFMArtist.isSavedArtist() = lastFMLocalStorage.getArtistById(id)
+
+    private fun markArtistAsLocal(artist: LastFMArtist) {
+        artist.isLocallyStored = true
+    }
+
+}
