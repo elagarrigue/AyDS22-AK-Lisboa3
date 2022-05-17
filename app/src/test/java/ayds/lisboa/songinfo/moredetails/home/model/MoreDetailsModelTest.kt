da108b8934037b81bd76f2fdd38bc457f4783e3a
Ignacio <ij.dotta@gmail.com>
2022-05-17 20:50:45 -0300

    MoreDetailsModelTest.kt implemented and passed

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/MoreDetailsModelTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/MoreDetailsModelTest.kt
@@ -1,4 +1,30 @@
 package ayds.lisboa.songinfo.moredetails.home.model
 
+import ayds.lisboa.songinfo.moredetails.home.model.entities.Artist
+import ayds.lisboa.songinfo.moredetails.home.model.repository.ArtistRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
 class MoreDetailsModelTest {
+
+    private val repository: ArtistRepository = mockk()
+
+    private val moreDetailsModel: MoreDetailsModel = MoreDetailsModelImpl(repository)
+
+    @Test
+    fun `on search artist it should notify the result`() {
+        val artist: Artist = mockk()
+        every { repository.getArtistByName("name") } returns artist
+        val artistTester: (Artist) -> Unit = mockk(relaxed = true)
+        moreDetailsModel.artistObservable.subscribe {
+            artistTester(artist)
+        }
+
+        moreDetailsModel.searchArtist("name")
+
+        verify { artistTester(artist) }
+    }
+
 }
