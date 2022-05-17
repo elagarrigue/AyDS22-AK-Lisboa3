6fb8e930e5712219761e529d3717c7b5fd7244a5
Ignacio <ij.dotta@gmail.com>
2022-05-17 20:55:05 -0300

    ArtistRepositoryTest.kt defined implementation common to all tests

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/repository/ArtistRepositoryTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/repository/ArtistRepositoryTest.kt
@@ -1,4 +1,17 @@
 package ayds.lisboa.songinfo.moredetails.home.model.repository
 
+import ayds.lisboa.songinfo.moredetails.home.model.repository.external.lastfm.LastFMService
+import ayds.lisboa.songinfo.moredetails.home.model.repository.local.lastfm.LastFMLocalStorage
+import io.mockk.mockk
+
 class ArtistRepositoryTest {
+
+    private val lastFMLocalStorage: LastFMLocalStorage = mockk(relaxUnitFun = true)
+    private val lastFMService: LastFMService = mockk(relaxUnitFun = true)
+
+    private val artistRepository: ArtistRepository = ArtistRepositoryImpl(
+        lastFMLocalStorage,
+        lastFMService
+    )
+
 }
