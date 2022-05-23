82f2701808450abecc5d98e2216f65f945409991
Thiago Zanconi <thiagzanc@gmail.com>
2022-05-23 09:04:33 -0300

    Added test case in ArtistRepositoryTest

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/repository/ArtistRepositoryTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/repository/ArtistRepositoryTest.kt
@@ -1,5 +1,6 @@
 package ayds.lisboa.songinfo.moredetails.home.model.repository
 
+import ayds.lisboa.songinfo.home.model.entities.EmptySong
 import ayds.lisboa.songinfo.home.model.entities.SpotifySong
 import ayds.lisboa.songinfo.moredetails.home.model.entities.EmptyArtist
 import ayds.lisboa.songinfo.moredetails.home.model.entities.LastFMArtist
@@ -7,6 +8,7 @@ import ayds.lisboa.songinfo.moredetails.home.model.repository.external.lastfm.La
 import ayds.lisboa.songinfo.moredetails.home.model.repository.local.lastfm.LastFMLocalStorage
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -41,6 +43,21 @@ class ArtistRepositoryTest {
 
         assertEquals(artist, result)
         assertTrue(artist.isLocallyStored)
+    }
+
+    @Test
+    fun `given non existing artist by name should get the artist and store it`() {
+        val artist = LastFMArtist(
+            "name", "url","info", false
+        )
+        every { lastFMLocalStorage.getArtistByName("name") } returns null
+        every { lastFMService.getArtist("name") } returns artist
+
+        val result = artistRepository.getArtistByName("name")
+
+        assertEquals(artist, result)
+        assertFalse(artist.isLocallyStored)
+        verify { lastFMLocalStorage.insertArtist(artist) }
     }
 
     @Test
