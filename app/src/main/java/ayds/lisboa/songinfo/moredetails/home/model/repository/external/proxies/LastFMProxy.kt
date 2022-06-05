96af100fcb71e10235e11626d81f003efc90011c
Dylan Hughes <dylan_21174@hotmail.com>
2022-06-05 20:10:47 -0300

    Implemented LastFMProxy

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/proxies/LastFMProxy.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/proxies/LastFMProxy.kt
@@ -1,12 +1,39 @@
 package ayds.lisboa.songinfo.moredetails.home.model.repository.external.proxies
 
 import ayds.lisboa.lastfmdata.lastfm.LastFMService
+import ayds.lisboa.lastfmdata.lastfm.entities.LastFMArtist
 import ayds.lisboa.songinfo.moredetails.home.model.entities.Card
+import ayds.lisboa.songinfo.moredetails.home.model.entities.CardImpl
+import ayds.lisboa.songinfo.moredetails.home.model.entities.EmptyCard
+import ayds.lisboa.songinfo.moredetails.home.model.entities.Source
 
-internal class LastFMProxy(private val lastFMService: LastFMService): ProxyService {
-
+internal class LastFMProxy(private val lastFMService: LastFMService) : ProxyService {
 
     override fun getCard(artist: String): Card {
-        TODO("Not yet implemented")
+
+        var artistCard: CardImpl? = null
+
+        try {
+            val serviceInfo: LastFMArtist? = lastFMService.getArtist(artist)
+            serviceInfo?.let {
+                artistCard = createCard(it)
+            }
+
+        } catch (e: Exception) {
+            artistCard = null
+        }
+
+        return artistCard ?: EmptyCard
     }
+
+    private fun createCard(serviceInfo: LastFMArtist) =
+        CardImpl(
+            serviceInfo.name,
+            serviceInfo.infoUrl,
+            serviceInfo.description,
+            false,
+            Source.LASTFM,
+            serviceInfo.sourceLogoUrl
+        )
+
 }
