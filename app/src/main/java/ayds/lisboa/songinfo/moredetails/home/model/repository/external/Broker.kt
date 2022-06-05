9255be9235a4324f00eb978d77d4483eeb2cfdac
Dylan Hughes <dylan_21174@hotmail.com>
2022-06-05 17:44:00 -0300

    BrokerImpl created

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/Broker.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/Broker.kt
@@ -5,3 +5,10 @@ import ayds.lisboa.songinfo.moredetails.home.model.entities.Card
 interface Broker {
     fun getInfoByArtistName(name: String): List<Card>
 }
+
+internal class BrokerImpl(): Broker{
+    override fun getInfoByArtistName(name: String): List<Card> {
+        TODO("Not yet implemented")
+    }
+
+}
