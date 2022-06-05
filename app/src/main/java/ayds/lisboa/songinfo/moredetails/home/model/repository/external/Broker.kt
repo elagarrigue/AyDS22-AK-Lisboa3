727ee3719cb0a7ebf4496b7e69e947abd852776e
Dylan Hughes <dylan_21174@hotmail.com>
2022-06-05 20:09:14 -0300

    Implemented Broker.kt

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/Broker.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/Broker.kt
@@ -1,14 +1,31 @@
 package ayds.lisboa.songinfo.moredetails.home.model.repository.external
 
 import ayds.lisboa.songinfo.moredetails.home.model.entities.Card
+import ayds.lisboa.songinfo.moredetails.home.model.entities.CardImpl
+import ayds.lisboa.songinfo.moredetails.home.model.repository.external.proxies.ProxyService
+import java.util.*
 
 interface Broker {
     fun getInfoByArtistName(name: String): List<Card>
 }
 
-internal class BrokerImpl(): Broker{
-    override fun getInfoByArtistName(name: String): List<Card> {
-        TODO("Not yet implemented")
+internal class BrokerImpl(private var proxyList: LinkedList<ProxyService>) : Broker {
+
+    private var infoList = LinkedList<CardImpl>()
+
+    override fun getInfoByArtistName(name: String): List<CardImpl> {
+
+        var card: Card
+        proxyList.forEach {
+            card = it.getCard(name)
+            insertCardFromProxy(card)
+        }
+        return infoList
+    }
+
+    private fun insertCardFromProxy(card: Card) {
+        if (card is CardImpl)
+            this.infoList.add(card)
     }
 
 }
