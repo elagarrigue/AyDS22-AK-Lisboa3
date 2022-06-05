6b2d35903b1c6dc227131f96698aac905eed52d7
Dylan Hughes <dylan_21174@hotmail.com>
2022-06-05 20:11:08 -0300

    Implemented NYTProxy

--- app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/proxies/NYTProxy.kt
+++ app/src/main/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/proxies/NYTProxy.kt
@@ -1,10 +1,38 @@
 package ayds.lisboa.songinfo.moredetails.home.model.repository.external.proxies
 
 import ayds.lisboa.songinfo.moredetails.home.model.entities.Card
+import ayds.lisboa.songinfo.moredetails.home.model.entities.CardImpl
+import ayds.lisboa.songinfo.moredetails.home.model.entities.EmptyCard
+import ayds.lisboa.songinfo.moredetails.home.model.entities.Source
 import ayds.ny3.newyorktimes.NytArticleService
+import ayds.ny3.newyorktimes.NytArtistInfo
 
-internal class NYTProxy(nytArticleService: NytArticleService): ProxyService {
+internal class NYTProxy(private val nytArticleService: NytArticleService) : ProxyService {
+
     override fun getCard(artist: String): Card {
-        TODO("Not yet implemented")
+
+        var artistCard: CardImpl? = null
+
+        try {
+            val serviceInfo: NytArtistInfo? = nytArticleService.getArtistInfo(artist)
+            serviceInfo?.let {
+                artistCard = createCard(it)
+            }
+
+        } catch (e: Exception) {
+            artistCard = null
+        }
+
+        return artistCard ?: EmptyCard
     }
+
+    private fun createCard(serviceInfo: NytArtistInfo) =
+        CardImpl(
+            serviceInfo.artistName,
+            serviceInfo.artistURL,
+            serviceInfo.artistInfo,
+            false,
+            Source.NEW_YORK_TIMES,
+            serviceInfo.nytLogo
+        )
 }
