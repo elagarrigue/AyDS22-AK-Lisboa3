b8ce11ebe185fc614acc3f8772c4c58d42261568
Ignacio <ij.dotta@gmail.com>
2022-06-07 21:07:45 -0300

    Broker now has a list of three proxies. Every test now verifies for every proxy its method is called.

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/BrokerTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/repository/external/BrokerTest.kt
@@ -7,32 +7,42 @@ import ayds.lisboa.songinfo.moredetails.home.model.entities.Source
 import ayds.lisboa.songinfo.moredetails.home.model.repository.external.proxies.ProxyService
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
 import org.junit.Assert.*
 
 class BrokerTest {
 
-    private val proxy: ProxyService = mockk()
-    private val broker: Broker = BrokerImpl(listOf(proxy))
+    private val proxies: List<ProxyService> = listOf(mockk(), mockk(), mockk())
+    private val broker: Broker = BrokerImpl(proxies)
 
     @Test
     fun `given artist name should return list of artist info card`() {
         val card: Card =
             CardImpl("name", "infoUrl", "description", false, Source.UNDEFINED, "logoUrl")
-        every { proxy.getCard("name") } returns card
+        proxies.forEach {
+            every { it.getCard("name") } returns card
+        }
 
         val result = broker.getInfoByArtistName("name")
 
-        assertEquals(listOf(card), result)
+        assertEquals(listOf(card, card, card), result)
+        proxies.forEach {
+            verify { it.getCard("name") }
+        }
     }
 
     @Test
     fun `given service exception should return empty list`() {
-        every { proxy.getCard("name") } returns EmptyCard
+        proxies.forEach {
+            every { it.getCard("name") } returns EmptyCard
+        }
 
         val result = broker.getInfoByArtistName("name")
 
         assertEquals(listOf<Card>(), result)
+        proxies.forEach {
+            verify { it.getCard("name") }
+        }
     }
-
 }
