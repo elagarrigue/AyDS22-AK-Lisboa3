b067c9271e5784c382c9ea163ba0010007ac9b97
Ignacio <ij.dotta@gmail.com>
2022-06-08 11:11:15 -0300

    Fixed MoreDetailsModelTest.kt. Added empty list of cards test.

--- app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/MoreDetailsModelTest.kt
+++ app/src/test/java/ayds/lisboa/songinfo/moredetails/home/model/MoreDetailsModelTest.kt
@@ -1,6 +1,7 @@
 package ayds.lisboa.songinfo.moredetails.home.model
 
 import ayds.lisboa.songinfo.moredetails.home.model.entities.Card
+import ayds.lisboa.songinfo.moredetails.home.model.entities.CardImpl
 import ayds.lisboa.songinfo.moredetails.home.model.repository.CardRepository
 import io.mockk.every
 import io.mockk.mockk
@@ -10,21 +11,38 @@ import org.junit.Test
 class MoreDetailsModelTest {
 
     private val repository: CardRepository = mockk()
-
     private val moreDetailsModel: MoreDetailsModel = MoreDetailsModelImpl(repository)
 
     @Test
     fun `on search artist it should notify the result`() {
-        val cards: List<Card> = mockk()
+        val card: CardImpl = mockk()
+        val cards: List<Card> = listOf(card)
+
         every { repository.getCardsByName("name") } returns cards
-        val artistTester: (List<Card>) -> Unit = mockk(relaxed = true)
+
+        val artistTester: (Card) -> Unit = mockk(relaxed = true)
         moreDetailsModel.cardObservable.subscribe {
             artistTester(it)
         }
 
         moreDetailsModel.searchCards("name")
 
-        verify { artistTester(cards) }
+        verify { artistTester(card) }
+    }
+
+    @Test
+    fun `on search artist it should not notify any result if no card is found`() {
+        val cards: List<Card> = listOf()
+
+        every { repository.getCardsByName("name") } returns cards
+
+        val artistTester: (Card) -> Unit = mockk(relaxed = true)
+        moreDetailsModel.cardObservable.subscribe {
+            artistTester(it)
+        }
+
+        moreDetailsModel.searchCards("name")
+        verify(exactly = 0) { artistTester(mockk()) }
     }
 
 }
